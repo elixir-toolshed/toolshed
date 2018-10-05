ae2b39546d992939fc3d66c42db707161c84e5b5
Connor Rigby <konnorrigby@gmail.com>
2018-10-05 13:00:35 -0700

    Add helper to save/load terms to/from fs

--- lib/my_helpers/misc.ex
+++ lib/my_helpers/misc.ex
@@ -26,4 +26,36 @@ defmodule MyHelpers.Misc do
     contents = inspect(value, opts)
     File.write(path, contents)
   end
+
+  @doc """
+  Save an Erlang term to the filesystem for easy loading later
+
+  This function is returns the `value` passed in to allow easy piping.
+
+  ## Examples
+    iex> :sys.get_state(MyServer) |> save_term!("/root/my_server.term")
+    # Reboot board
+    iex> :sys.replace_state(&load_term!("/root/my_server.term"))
+  """
+  @spec save_term!(term, Path.t()) :: term
+  def save_term!(value, path) do
+    term = :erlang.term_to_binary(value)
+    :ok = File.write!(path, term)
+    value
+  end
+
+  @doc """
+  Load an Erlgn term from the filesystem.
+
+  ## Examples
+    iex> save_term!({:some_interesting_atom, ["some", "list"]}, "/root/some_atom.term")
+    {:some_interesting_atom, ["some", "list"]}
+    iex> load_term!("/root/some_atom.term")
+    {:some_interesting_atom, ["some", "list"]}
+  """
+  def load_term!(path) do
+    path
+    |> File.read!()
+    |> :erlang.binary_to_term()
+  end
 end
