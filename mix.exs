cc3e1bffde322f6709200d010958cf5091a3d446
Frank Hunleth <fhunleth@troodon-software.com>
2018-11-01 20:55:42 -0400

    v0.2.2 release

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Toolshed.MixProject do
   def project do
     [
       app: :toolshed,
-      version: "0.2.1",
+      version: "0.2.2",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
