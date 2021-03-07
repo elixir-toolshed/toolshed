cb1ca447709d31e8e6feb18ced854f382845ccb7
okothkongo <okothkongo@gmail.com>
2021-03-07 12:20:24 +0300

    Add test for date helper
    This adds a unit test for the date helper function. The Unix date command adds an
    extra space for single digit days so this updates the implementation to do that too.
    
    test date in unix format
    
    use system cmd to test date
    
    make date function behave like unix date command
    
    resolve suggestions

--- lib/toolshed/unix.ex
+++ lib/toolshed/unix.ex
@@ -65,36 +65,45 @@ defmodule Toolshed.Unix do
   end
 
   @doc """
-  Print out the date similar to the Unix date command
+  Return the date and time in UTC
   """
   @spec date() :: String.t()
   def date() do
-    dt = DateTime.utc_now()
-
-    "#{weekday_text(dt)} #{month_text(dt)} #{dt.day} #{time_text(dt)} UTC #{dt.year}"
+    date_in_unix_format(DateTime.utc_now())
   end
 
-  defp weekday_text(dt) do
-    day_index = dt |> DateTime.to_date() |> Date.day_of_week()
-
-    elem(
-      {"", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"},
-      day_index
-    )
+  defp weekday_text(date_time) do
+    date_time
+    |> DateTime.to_date()
+    |> Date.day_of_week()
+    |> weekday()
   end
 
-  defp month_text(dt) do
+  defp weekday(day_in_number) do
+    elem({"", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"}, day_in_number)
+  end
+
+  defp month_text(date_time) do
     elem(
       {"", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"},
-      dt.month
+      date_time.month
     )
   end
 
-  defp time_text(dt) do
-    dt
+  defp time_text(date_time) do
+    date_time
     |> DateTime.to_time()
     |> Time.truncate(:second)
     |> Time.to_string()
+  end
+
+  defp format_date(date) when date > 9, do: date
+  defp format_date(date), do: " #{date}"
+
+  defp date_in_unix_format(date_time) do
+    "#{weekday_text(date_time)} #{month_text(date_time)} #{format_date(date_time.day)} #{
+      time_text(date_time)
+    } UTC #{date_time.year}"
   end
 
   defp do_tree(_prefix, _dir, []), do: :ok
