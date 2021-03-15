eba9c93d4c2c5d6a3caa67a7942f01040520ffd3
okothkongo <okothkongo@gmail.com>
2021-03-15 20:07:55 +0300

    This adds a unit test for the cat helper function.
    It also changes the IO use write/1 instead of puts/1 due to an extra space produce by puts/1
    this is due the fact that actual Unix cat command do not produce extra line.
    And it  adds alittle information to the cat helper function definition
    to indicate it prints the content of a file.

--- lib/toolshed/unix.ex
+++ lib/toolshed/unix.ex
@@ -14,11 +14,14 @@ defmodule Toolshed.Unix do
   """
 
   @doc """
-  Print out a file
+  Reads and prints out the contents of a file
   """
   @spec cat(Path.t()) :: :"do not show this result in output"
   def cat(path) do
-    File.read!(path) |> IO.puts()
+    path
+    |> File.read!()
+    |> IO.write()
+
     IEx.dont_display_result()
   end
 
