94c66f98c307d7d770b364361361982085466f32
okothkongo <okothkongo@gmail.com>
2021-03-15 20:07:55 +0300

    This adds a unit test for the cat helper function.
    It also changes the IO use write/1 instead of puts/1 due to an extra space produce by puts/1
    this is due the fact that actual Unix cat command do not produce extra line.
    And it  adds alittle information to the cat helper function definition
    to indicate it prints the content of a file.

--- test/toolshed/unix_test.exs
+++ test/toolshed/unix_test.exs
@@ -1,5 +1,6 @@
 defmodule Toolshed.UnixTest do
   use ExUnit.Case
+  import ExUnit.CaptureIO
   alias Toolshed.Unix
 
   test "date/0 returns current date in unix format" do
@@ -9,5 +10,10 @@ defmodule Toolshed.UnixTest do
   defp unix_date_output do
     {date_time, 0} = System.cmd("date", ["-u"], env: %{"LC_ALL" => "C"})
     String.trim(date_time)
+  end
+
+  test "cat/1 reads file content" do
+    assert capture_io(fn -> Unix.cat("test/support/test_file.doc") end) ==
+             "Content of this will be read for test purposes"
   end
 end
