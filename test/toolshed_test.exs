ffe816c8bd4260cedd6a02ecefc6ca71e3d3a310
Connor Rigby <connorr@hey.com>
2021-03-24 10:48:25 -0700

    Update cmd/1 to use `binstream` instead of `stream`
    This is needed to prevent crashing when a command
    writes non-unicode values to stdout.

--- test/toolshed_test.exs
+++ test/toolshed_test.exs
@@ -1,3 +1,16 @@
 defmodule ToolshedTest do
   use ExUnit.Case
+  import ExUnit.CaptureIO
+
+  test "cmd/1 normal printable chars" do
+    assert capture_io(fn ->
+             Toolshed.cmd("echo \"hello, world\"")
+           end) == "hello, world\n"
+  end
+
+  test "cmd/1 non printable chars" do
+    assert capture_io(fn ->
+             Toolshed.cmd("echo -e -n '\\x0'")
+           end) == <<0>>
+  end
 end
