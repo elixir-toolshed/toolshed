c1302297de2fa7b8475c7f60f5b1de2d504e9a1e
okothkongo <okothkongo@gmail.com>
2021-04-09 12:06:45 +0300

    Move date function to its own module
    This moves date/0 function from Toolshed.Unix module to its independent
    module(Toolshed.Date) to bring command per module consistency.

---
+++ test/toolshed/date_test.exs
@@ -0,0 +1,19 @@
+defmodule Toolshed.DateTest do
+  use ExUnit.Case
+
+  test "date/0 returns current date in unix format" do
+    # There's a race condition on the time that's returned between the
+    # two date functions if we catch the seconds changing.
+
+    expected1 = unix_date_output()
+    actual = Toolshed.Date.date()
+    expected2 = unix_date_output()
+
+    assert actual == expected1 or actual == expected2
+  end
+
+  defp unix_date_output do
+    {date_time, 0} = System.cmd("date", ["-u"], env: %{"LC_ALL" => "C"})
+    String.trim(date_time)
+  end
+end
--- test/toolshed/unix_test.exs
+++ test/toolshed/unix_test.exs
@@ -3,22 +3,6 @@ defmodule Toolshed.UnixTest do
   import ExUnit.CaptureIO
   alias Toolshed.Unix
 
-  test "date/0 returns current date in unix format" do
-    # There's a race condition on the time that's returned between the
-    # two date functions if we catch the seconds changing.
-
-    expected1 = unix_date_output()
-    actual = Unix.date()
-    expected2 = unix_date_output()
-
-    assert actual == expected1 or actual == expected2
-  end
-
-  defp unix_date_output do
-    {date_time, 0} = System.cmd("date", ["-u"], env: %{"LC_ALL" => "C"})
-    String.trim(date_time)
-  end
-
   test "cat/1 reads file content" do
     assert capture_io(fn -> Unix.cat("test/support/test_file.doc") end) ==
              "Content of this will be read for test purposes"
