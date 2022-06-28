3bd4e0f56ac01c49128edd2e2bc97e60c8919067
Masatoshi N <nishiguchi.masa@gmail.com>
2022-06-28 07:44:09 -0400

    Add minimal tests for HTTP

--- test/toolshed/http_test.exs
+++ test/toolshed/http_test.exs
@@ -10,4 +10,12 @@ defmodule Toolshed.HTTPTest do
              )
            end) =~ "Toolshed aims to improve the Elixir shell"
   end
+
+  test "weather/0 returns correct value" do
+    assert Toolshed.HTTP.weather() == :"do not show this result in output"
+  end
+
+  test "qr_encode/1 returns correct value" do
+    assert Toolshed.HTTP.qr_encode("Nerves") == :"do not show this result in output"
+  end
 end
