a51ad6580d232c45a1a4d359af09b3f742bfcacc
Masatoshi N <nishiguchi.masa@gmail.com>
2022-09-06 19:56:47 -0400

    Split Toolshed.Misc into per-fun mods

--- test/toolshed/save_value_test.exs
+++ test/toolshed/save_value_test.exs
@@ -1,5 +1,7 @@
-defmodule Toolshed.MiscTest do
+defmodule Toolshed.SaveValueTest do
   use ExUnit.Case
+
+  import Toolshed.SaveValue
 
   # Default tmp dir does not work in CI
   @moduletag tmp_dir: System.tmp_dir!()
@@ -8,16 +10,8 @@ defmodule Toolshed.MiscTest do
     path = tmp_file(context.tmp_dir, "text_data")
     content = {:some_interesting_atom, ["some", "list"]}
 
-    assert Toolshed.Misc.save_value(content, path) == :ok
+    assert save_value(content, path) == :ok
     assert File.read!(path) == "{:some_interesting_atom, [\"some\", \"list\"]}"
-  end
-
-  test "save_term!/2 saves Elixir term as binary", context do
-    path = tmp_file(context.tmp_dir, "binary_data")
-    content = {:some_interesting_atom, ["some", "list"]}
-
-    assert Toolshed.Misc.save_term!(content, path) == content
-    assert Toolshed.Misc.load_term!(path) == content
   end
 
   defp tmp_file(tmp_dir, filename) do
