0006ad713ec5242df414ef89f32b0eb54990645b
Masatoshi N <nishiguchi.masa@gmail.com>
2022-09-13 20:44:38 -0400

    Simplify public function test

--- test/toolshed_test.exs
+++ test/toolshed_test.exs
@@ -2,38 +2,42 @@ defmodule ToolshedTest do
   use ExUnit.Case
   import ExUnit.CaptureIO
 
-  @toolshed_commands [
-    cat: 1,
-    cmd: 1,
-    date: 0,
-    grep: 2,
-    hex: 1,
-    history: 1,
-    hostname: 0,
-    httpget: 2,
-    ifconfig: 0,
-    load_term!: 1,
-    log_attach: 1,
-    log_detach: 0,
-    lsof: 0,
-    lsusb: 0,
-    multicast_addresses: 0,
-    nslookup: 1,
-    ping: 2,
-    qr_encode: 1,
-    save_term!: 2,
-    save_value: 3,
-    top: 1,
-    tping: 2,
-    tree: 1,
-    uptime: 0,
-    weather: 0
-  ]
-
   test "public functions" do
-    actual = Toolshed.__info__(:functions)
-
-    for f <- @toolshed_commands, do: assert(f in actual)
+    assert Toolshed.__info__(:functions) == [
+             {:cat, 1},
+             {:cmd, 1},
+             {:date, 0},
+             {:grep, 2},
+             {:hex, 1},
+             {:history, 0},
+             {:history, 1},
+             {:hostname, 0},
+             {:httpget, 1},
+             {:httpget, 2},
+             {:ifconfig, 0},
+             {:load_term!, 1},
+             {:log_attach, 0},
+             {:log_attach, 1},
+             {:log_detach, 0},
+             {:lsof, 0},
+             {:lsusb, 0},
+             {:multicast_addresses, 0},
+             {:nslookup, 1},
+             {:ping, 1},
+             {:ping, 2},
+             {:qr_encode, 1},
+             {:save_term!, 2},
+             {:save_value, 2},
+             {:save_value, 3},
+             {:top, 0},
+             {:top, 1},
+             {:tping, 1},
+             {:tping, 2},
+             {:tree, 0},
+             {:tree, 1},
+             {:uptime, 0},
+             {:weather, 0}
+           ]
   end
 
   test "cmd/1 normal printable chars" do
