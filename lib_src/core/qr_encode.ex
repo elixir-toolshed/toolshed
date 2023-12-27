5bb60e8e49440083dc3238d52afb42ae758ec4e4
Jon Carstens <jjcarstens@me.com>
2023-12-26 20:29:27 -0700

    Add `qr_encode_wifi`
    Simple helper to format WiFi credentials with QR code formats which
    can be used on mobile devices to directly connect

--- lib_src/core/qr_encode.ex
+++ lib_src/core/qr_encode.ex
@@ -14,13 +14,29 @@ defmodule Toolshed.Core.QrEncode do
     {:ok, {_status, _headers, body}} =
       :httpc.request(
         :post,
-        {'http://qrenco.de/', [{'User-Agent', 'curl'}], 'application/x-www-form-urlencoded',
-         form_data},
+        {~c"http://qrenco.de/", [{~c"User-Agent", ~c"curl"}],
+         ~c"application/x-www-form-urlencoded", form_data},
         [],
         []
       )
 
     body |> :binary.list_to_bin() |> IO.puts()
     IEx.dont_display_result()
+  end
+
+  @doc """
+  Generate an ASCII art QR code for WiFi connections
+
+  See https://en.wikipedia.org/wiki/QR_code#Joining_a_Wi%E2%80%91Fi_network
+  for string format
+  """
+  @type wifi_opt :: {:hidden, boolean()} | {:type, :WPA | :WEP | :nopass}
+  @spec qr_encode_wifi(String.t(), String.t(), [wifi_opt()]) ::
+          :"do not show this result in output"
+  def qr_encode_wifi(ssid, psk, opts \\ []) do
+    type = opts[:type] || "WPA"
+    hidden = opts[:hidden] || false
+    msg = "WIFI:S:#{ssid};T:#{type};P:#{psk};H:#{hidden};;"
+    qr_encode(msg)
   end
 end
--- test/toolshed/qr_encode_test.exs
+++ test/toolshed/qr_encode_test.exs
@@ -4,4 +4,9 @@ defmodule Toolshed.QrEncodeTest do
   test "qr_encode/1 returns correct value" do
     assert Toolshed.qr_encode("Nerves") == :"do not show this result in output"
   end
+
+  test "qr_encode_wifi/3 returns correct value" do
+    assert Toolshed.qr_encode_wifi("NervesSSID", "NervesIsCool", hidden: false, type: :WPA) ==
+             :"do not show this result in output"
+  end
 end
