1f67b044e75f953b32b6d4245f412898568387e2
Frank Hunleth <fhunleth@troodon-software.com>
2025-07-01 13:50:46 -0400

    cmd: Add UTF8 sanitizer to work with OTP 28's unicode-only ssh client
    Previously a hack was in place to output the raw bytes coming from
    system commands. If that wasn't done, then IO.write would raise an
    ArgumentException when non-UTF8 data was output. That was super
    unfriendly and raw streams worked fine.
    
    OTP 28 no longer supports non-unicode via the SSH client. This could
    be fixed, but properly filtering out bad characters seems reasonable
    too. This also gets rid of all of the command output being recorded and
    then thrown away. It's now thrown away immediately. This should help
    memory use when running a command that dumps a lot of data.

--- lib_src/core/cmd.ex
+++ lib_src/core/cmd.ex
@@ -1,4 +1,45 @@
 defmodule Toolshed.Core.Cmd do
+  defmodule SanitizerStream do
+    @moduledoc false
+    defstruct []
+
+    defimpl Collectable, for: SanitizerStream do
+      @moduledoc false
+
+      if System.version() >= "1.16" do
+        @impl Collectable
+        def into(%SanitizerStream{}) do
+          collector_fun =
+            fn
+              _, {:cont, data} ->
+                data
+                |> IO.iodata_to_binary()
+                |> String.replace_invalid()
+                |> IO.write()
+
+              _, _ ->
+                :ok
+            end
+
+          {:ok, collector_fun}
+        end
+      else
+        # For Elixir 1.15 and earlier, just do what we used to which was to do
+        # a raw write and hope for the best.
+        @impl Collectable
+        def into(%SanitizerStream{}) do
+          collector_fun =
+            fn
+              _, {:cont, data} -> IO.binwrite(data)
+              _, _ -> :ok
+            end
+
+          {:ok, collector_fun}
+        end
+      end
+    end
+  end
+
   @doc """
   Run a command and return the exit code. This function is intended to be run
   interactively.
@@ -6,7 +47,10 @@ defmodule Toolshed.Core.Cmd do
   @spec cmd(String.t() | charlist()) :: integer()
   def cmd(str) when is_binary(str) do
     {_collectable, exit_code} =
-      System.cmd("sh", ["-c", str], stderr_to_stdout: true, into: IO.binstream(:stdio, :line))
+      System.cmd("sh", ["-c", str],
+        stderr_to_stdout: true,
+        into: %Toolshed.SanitizerStream{}
+      )
 
     exit_code
   end
---
+++ test/toolshed/cmd_test.exs
@@ -0,0 +1,16 @@
+defmodule Toolshed.CmdTest do
+  use ExUnit.Case
+  import ExUnit.CaptureIO
+
+  test "cmd/1 can run a program" do
+    assert capture_io(fn -> Toolshed.cmd("cat test/support/test_file.doc") end) ==
+             "Content of this will be read for test purposes"
+  end
+
+  test "cmd/1 doesn't crash on invalid UTF-8" do
+    # The invalid UTF-8 characters are replaced by � in Elixir 1.16 and later.
+    assert capture_io(fn ->
+             Toolshed.cmd("echo 'invalid \xFF\xFE utf8'")
+           end) =~ ~r/invalid .* utf8/
+  end
+end
